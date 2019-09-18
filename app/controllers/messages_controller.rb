3e053b5d8568b18ede87988fb21b10add6898a23
hisakolo <iune30.poakolu@gmail.com>
2019-09-18 21:49:28 +0900

    group-chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,23 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
 end
