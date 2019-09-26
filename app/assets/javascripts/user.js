e471463d5d31f9ecffbb3ec248873f79bd95b0fd
hisakolo <iune30.poakolu@gmail.com>
2019-09-26 22:06:04 +0900

    Update user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -7,7 +7,6 @@ $(function() {
                 <p class="chat-group-user__name">${user.name}</p>
                 <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
               </div>`
-  // $("#user-search-result").append(html)
   search_list.append(html);
 }
 
@@ -32,7 +31,6 @@ function addUser(name,user_id) {
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    console.log(input)
 
     $.ajax({
       type: 'GET',
@@ -43,14 +41,12 @@ function addUser(name,user_id) {
     .done(function(user) {
       $("#user-search-result").empty();
         if (user.length !== 0) {
-          console.log("上のじょうけん");
           user.forEach(function(user){
           appendUser(user);
           });
         }
         else {
           appendErrMsgToHTML("一致するユーザーはいません");
-          console.log("下の条件");
         }
     })
     .fail(function() {
@@ -59,7 +55,6 @@ function addUser(name,user_id) {
   });
 
   $("#user-search-result").on("click", ".user-search-add",function() {
-    console.log(this)
     var name = $(this).data("user-name");
     var user_id = $(this).data("user-id");
     addUser(name,user_id);
@@ -67,10 +62,7 @@ function addUser(name,user_id) {
   });
 
   $(".chat-group-users").on('click', '.user-search-remove', function() {
-    console.log(this)
     var user_id = $(this).data('user-id');
-    // addUser(name,user_id);
     $(this).parent().remove();
-    // $(`#chat-group-user-${user_id}`).remove();
   });
 });
