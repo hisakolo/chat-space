a5741d19fad6cb06a5ca2b60245ecd3de8d759e1
hisakolo <iune30.poakolu@gmail.com>
2019-10-01 20:37:58 +0900

    Update 20190917074223_devise_create_users.rb

--- db/migrate/20190917074223_devise_create_users.rb
+++ db/migrate/20190917074223_devise_create_users.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
@@ -7,6 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.timestamps null: false
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.datetime :remember_created_at
     end
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
   end
 end
