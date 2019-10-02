23f4b2d8ce1456240464c1ab4321e573858973e9
hisakolo <iune30.poakolu@gmail.com>
2019-10-02 14:38:57 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module ChatSpace
   class Application < Rails::Application
     config.generators do |g|
     config.i18n.default_locale = :ja
-    config.time_zone = ’Tokyo’
+    config.time_zone =  'Tokyo'
     config.active_record.default_timezone = :utc
       g.stylesheets false
       g.javascripts false
